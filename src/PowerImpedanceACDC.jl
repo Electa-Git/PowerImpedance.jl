66aeee62c115221a76fa4a5748425dbd4207512f
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2026-01-21 16:39:21 +0100

    refactor(core): remove SymEngine import and dependencies
    Remove SymEngine import and all related code from the main module. This commit is part of a refactor in the components architecture to remove the dependence on the SymEngine.

--- src/PowerImpedanceACDC.jl
+++ src/PowerImpedanceACDC.jl
@@ -9,98 +9,97 @@ This program is free software: you can redistribute it and/or modify it under th
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
 """
-    # Default struct values
-    using Parameters
-    using DataStructures
+# Default struct values
+using Parameters
+using DataStructures
 
-    # Time delays
-    using ControlSystemsBase, RobustAndOptimalControl
+# Time delays
+using ControlSystemsBase, RobustAndOptimalControl
 
-    # Plotting
-    using Plots
-    using LaTeXStrings
+# Plotting
+using Plots
+using LaTeXStrings
 
-    # Symbolic and numerical calculations
-    using SymEngine
-    using LinearAlgebra
-    using ForwardDiff, NonlinearSolve, SteadyStateDiffEq  # solve diffs and nonlinear equations
-    
-    # Power flow
-    using PowerModels, PowerModelsACDC
-    using Ipopt
-    using JuMP
+# Symbolic and numerical calculations
+using LinearAlgebra
+using ForwardDiff, NonlinearSolve, SteadyStateDiffEq  # solve diffs and nonlinear equations
 
-    # For tools
-    using Peaks
-    using LsqFit
+# Power flow
+using PowerModels, PowerModelsACDC
+using Ipopt
+using JuMP
 
-    # Miscellaneous
-    using Munkres
-    using Markdown
-    using Interpolations
-    using DelimitedFiles
+# For tools
+using Peaks
+using LsqFit
 
-
-    # Including all components    
-    include("Network/Components/AbstractElement.jl")
-
-    # Impedance
-    include("Network/Components/impedance/impedance.jl")
-
-    # Transfromer
-    include("Network/Components/transformer/transformer.jl")
-
-    # Cables and transmission lines
-    include("Network/Components/transmission_line/transmission_line.jl")
-    include("Network/Components/transmission_line/cable.jl")
-    include("Network/Components/transmission_line/overhead_line.jl")
-    include("Network/Components/transmission_line/blackbox_line.jl")
-
-    # Grid or source
-    include("Network/Components/source/source.jl")
-    include("Network/Components/source/dc_source.jl")
-    include("Network/Components/source/ac_source.jl")
-
-    # Converter
-    include("Network/Components/converter/converter.jl")
-    include("Network/Components/converter/controller.jl")
-    include("Network/Components/converter/MMC.jl")
-    include("Network/Components/converter/TLC.jl")
-    include("Network/Components/converter/blackbox_MMC.jl")
-
-    # Machines
-    include("Network/Components/machine/machine.jl")
-    include("Network/Components/machine/SynchronousMachine.jl")
+# Miscellaneous
+using Munkres
+using Markdown
+using Interpolations
+using DelimitedFiles
 
 
-    # Including network
-    include("Network/Network.jl")
+# Including all components    
+include("Network/Components/AbstractElement.jl")
 
-    # New power flow
-    include("Network/power_flow.jl")
-    
-    # Including network solvers
-    include("Network/Solvers/make_abcd.jl")
-    include("Network/Solvers/make_y.jl")
-    include("Network/Solvers/make_z.jl")
-    include("Network/Solvers/determine_impedance.jl")
-    include("Network/Solvers/make_y_matrix.jl")
-    include("Network/Solvers/make_y_edge.jl")
-    include("Network/Solvers/make_y_edge_old.jl")
-    include("Network/Solvers/make_y_node.jl")
-    include("Network/Solvers/stability.jl")
+# Impedance
+include("Network/Components/impedance/impedance.jl")
 
-    # Including tools
-    include("Tools/abcd_parameters.jl")
-    include("Tools/kron.jl")
-    include("Tools/nyquistplot.jl")
-    include("Tools/small_gain.jl")
-    include("Tools/stabilitymargin.jl")
-    include("Tools/EVD.jl")
-    include("Tools/bodeplot.jl")
-    include("Tools/passivity.jl")
-    include("Tools/unstable_frequency.jl")
+# Transfromer
+include("Network/Components/transformer/transformer.jl")
 
-    
-    
+# Cables and transmission lines
+include("Network/Components/transmission_line/transmission_line.jl")
+include("Network/Components/transmission_line/cable.jl")
+include("Network/Components/transmission_line/overhead_line.jl")
+include("Network/Components/transmission_line/blackbox_line.jl")
+
+# Grid or source
+include("Network/Components/source/source.jl")
+include("Network/Components/source/dc_source.jl")
+include("Network/Components/source/ac_source.jl")
+
+# Converter
+include("Network/Components/converter/converter.jl")
+include("Network/Components/converter/controller.jl")
+include("Network/Components/converter/MMC.jl")
+include("Network/Components/converter/TLC.jl")
+include("Network/Components/converter/blackbox_MMC.jl")
+
+# Machines
+include("Network/Components/machine/machine.jl")
+include("Network/Components/machine/SynchronousMachine.jl")
+
+
+# Including network
+include("Network/Network.jl")
+
+# New power flow
+include("Network/power_flow.jl")
+
+# Including network solvers
+include("Network/Solvers/make_abcd.jl")
+include("Network/Solvers/make_y.jl")
+include("Network/Solvers/make_z.jl")
+include("Network/Solvers/determine_impedance.jl")
+include("Network/Solvers/make_y_matrix.jl")
+include("Network/Solvers/make_y_edge.jl")
+include("Network/Solvers/make_y_edge_old.jl")
+include("Network/Solvers/make_y_node.jl")
+include("Network/Solvers/stability.jl")
+
+# Including tools
+include("Tools/abcd_parameters.jl")
+include("Tools/kron.jl")
+include("Tools/nyquistplot.jl")
+include("Tools/small_gain.jl")
+include("Tools/stabilitymargin.jl")
+include("Tools/EVD.jl")
+include("Tools/bodeplot.jl")
+include("Tools/passivity.jl")
+include("Tools/unstable_frequency.jl")
+
+
+
 end
