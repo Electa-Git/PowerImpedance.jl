302963dbf184377f3607673243e67733eb04b535
u0158626 <jan.kircheis@kuleuven.be>
2026-01-24 17:35:33 +0100

    Upgrade of transformer unit testing

--- test/Transformer_test.jl
+++ test/Transformer_test.jl
@@ -5,6 +5,8 @@ Lₚ=0.1
 Lₛ=0.05
 Rₚ=10
 Rₛ=1
+Rᵪ=1e4
+Lᵪ=10
 n=320/400
 
 omegas = collect(range(2*pi*0.1, stop=2*pi*5000, step=10))
@@ -18,7 +20,7 @@ grid = Network()
 add!(
 	grid,
 	:labanimal,
-    transformer(n = n , Lₚ = Lₚ, Rₚ = Rₚ, Rₛ = Rₛ, 
+    transformer(n = n , Lₚ = Lₚ, Rₚ = Rₚ, Rₛ = Rₛ, Rₘ = Rᵪ, Lₘ = Lᵪ,
     Lₛ = Lₛ,
     pins = 3, transformation = true)
 )
@@ -45,12 +47,19 @@ for i in eachindex(omegas)
     ABCD_s=[Diagonal([1,1,1]) Z_s;
       zeros(3,3) Diagonal([1,1,1])]
 
+    Y_core=[ (1/Rᵪ + 1/(1im*ω*Lᵪ)) 0 0;
+             0 (1/Rᵪ + 1/(1im*ω*Lᵪ)) 0;
+             0 0 (1/Rᵪ + 1/(1im*ω*Lᵪ))]
+
+    ABCD_core=[Diagonal([1,1,1]) zeros(3,3);
+               Y_core Diagonal([1,1,1])]
+
 
     ABCD_n=[Diagonal([n,n,n]) zeros(3,3);
             zeros(3,3) Diagonal([1/n,1/n,1/n])]
 
     # Analytical transformer model
-    push!(ABCD_analytical, ABCD_p*ABCD_n*ABCD_s)
+    push!(ABCD_analytical, ABCD_p*ABCD_core*ABCD_n*ABCD_s)
     push!(ABCD_numerical, PowerImpedanceACDC.eval_abcd(grid.elements[:labanimal].element_value, 1im*ω))
 
 
